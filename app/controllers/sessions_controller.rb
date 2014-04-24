707a76a1d66fd14ba02a8c46cf794647550b1c7f
sercangedik <sercangedik@gmail.com>
2014-04-24 22:25:29 +0300

    sessions hata ekrani düzenlendi

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,16 @@ class SessionsController < ApplicationController
   end
   
   def create
-  
+    user = User.authenticate(params[:session][:email],
+                             params[:session][:password])
+   if user.nil?
+     flash.now[:error] = "Invalid email or password."
+     @title = "Sign In"
+   else
+     # success handling !
+   end
   end
+  
   
   def destroy
     
