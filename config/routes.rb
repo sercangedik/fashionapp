96f339c44daccec43f78e2bd76c0c60891ee2caa
sercangedik <sercangedik@gmail.com>
2014-04-24 22:20:11 +0300

    Session controlleri eklendi

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Fashion::Application.routes.draw do
   
+  get "sessions/new"
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
   
     #get "users/new"
   
@@ -9,6 +11,8 @@ Fashion::Application.routes.draw do
   get '/contact', :to => "pages#contact"
   get '/about', :to => "pages#about"
   get '/signup', :to => "users#new"
+  get '/signin', :to => "sessions#new"
+  get '/signout', :to => "sessions#destroy"
 
 
 
