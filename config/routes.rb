ef891abd5eaa0496d0d2b6ac09cfd08a59cbde1e
sercangedik <sercangedik@gmail.com>
2014-05-04 21:51:04 +0300

    post ekleme ekrani eklendi,validationlar düzeltildi

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Fashion::Application.routes.draw do
  # get "sessions/new"
   resources :users
   resources :sessions, :only => [:new, :create, :destroy]
+  resources :microposts, :only => [:create, :destroy]
   
     #get "users/new"
   
