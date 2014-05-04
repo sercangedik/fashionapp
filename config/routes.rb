53fc7bbb4893c7ddbde3e19a6eeb9ef20eae0225
sercangedik <sercangedik@gmail.com>
2014-05-05 00:27:01 +0300

    Takiplesme ozelliginin temelleri eklendi

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Fashion::Application.routes.draw do
   
  # get "sessions/new"
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  
   resources :sessions, :only => [:new, :create, :destroy]
   resources :microposts, :only => [:create, :destroy]
-  
+  resources :relationships, :only => [:create, :destroy]
     #get "users/new"
   
   root :to => "pages#home"
