e62a0ef122da6ec6131c8499b9a291bbe35c5cdd
sercangedik <sercangedik@gmail.com>
2014-05-12 02:37:48 +0300

    Revert "Postlarin takip eden kullanciliarda gozukmesi eklendi"
    This reverts commit 7fda2a1dbafb5085770c68e119014c61ccebb231.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -11,16 +11,4 @@ class Micropost < ActiveRecord::Base
   has_attached_file :image , styles: { :medium => "300x300>", :thumb => "100x100>" }
   
   default_scope :order => 'microposts.created_at DESC'
-  
-  scope :from_users_followed_by, lambda { |user| followed_by(user) }
-  
-  private
-  
-  def self.followed_by(user)
-    followed_ids = %(SELECT followed_id FROM relationships 
-                     WHERE follower_id = :user_id)
-                     where("user_id IN (#{followed_ids}) OR user_id = :user_id",
-                     :user_id => user)
-  end
 end
-
