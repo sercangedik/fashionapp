5b31737e34b6fade2f539ed81c52053707a38261
sercangedik <sercangedik@gmail.com>
2014-05-12 02:41:07 +0300

    Postlarin takip eden kullanciliarda gozukmesi eklendi"""
    This reverts commit 9dbbef6ee894d5f12905e68eea4aded10dfd4fa4.

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,15 +28,12 @@ class User < ActiveRecord::Base
                        
   before_save :encrypt_password
   
-  scope :admin, where(:admin => true)
-  
-  
   def has_password?(submitted_password)
     encrypted_password == encrypt(submitted_password)
   end
   
   def feed
-    Micropost.from_users_followed_by(self)
+    Micropost.where("user_id = ?",id)
   end
   
   def following?(followed)
